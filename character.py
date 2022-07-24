645653c52cab3afc71b8112e1abe7766708b2b5a
TuplexGaming <99618518+TuplexGaming@users.noreply.github.com>
2022-07-23 18:03:22 -0600

    Add primary stat setters, update(), toggle_regen()

--- character.py
+++ character.py
@@ -1,5 +1,4 @@
 from random import randint
-from threading import Timer
 
 
 class Character:
@@ -16,6 +15,10 @@ class Character:
     REGEN_INTERVAL = 1.0  # health/mana regen interval in seconds
 
     def __init__(self,
+                 x: int = 0,
+                 y: int = 0,
+                 dx: int = 0,
+                 dy: int = 0,
                  strength: int = None,
                  vitality: int = None,
                  intel: int = None,
@@ -27,114 +30,123 @@ class Character:
 
         Base stats should be integers between 3 and 18 (3d6).
         """
-        self.strength = strength if strength is not None else randint(self.STAT_MIN, self.STAT_MAX)
-        self.vitality = vitality if vitality is not None else randint(self.STAT_MIN, self.STAT_MAX)
-        self.intel = intel if intel is not None else randint(self.STAT_MIN, self.STAT_MAX)
-        self.dexterity = dexterity if dexterity is not None else randint(self.STAT_MIN, self.STAT_MAX)
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+        self.__strength = strength if strength is not None else randint(self.STAT_MIN, self.STAT_MAX)
+        self.__vitality = vitality if vitality is not None else randint(self.STAT_MIN, self.STAT_MAX)
+        self.__intel = intel if intel is not None else randint(self.STAT_MIN, self.STAT_MAX)
+        self.__dexterity = dexterity if dexterity is not None else randint(self.STAT_MIN, self.STAT_MAX)
         self.items = items
+        self.update_derived_stats()
+        self._regen = True  # Health and mana regeneration are on by default
 
+    def __str__(self):
+        char_info = f"""
+            x {self.x} y {self.y}
+            str {self.strength} vit {self.vitality} int {self.intel} dex {self.dexterity}
+            health {self.health} health regen {self.health_regen}
+            mana {self.mana} mana regen {self.mana_regen}
+            armor {self.armor} damage mult {self.damage_multiplier}
+            regen state {self._regen}
+            items {self.items}
+        """
+        return char_info
+
+    @property
+    def strength(self):
+        """Returns strength"""
+        return self.__strength
+
+    @strength.setter
+    def strength(self, value):
+        self.__strength = value
+        self.update_derived_stats()
+
+    @property
+    def vitality(self):
+        """Returns vitality"""
+        return self.__vitality
+
+    @vitality.setter
+    def vitality(self, value):
+        self.__vitality = value
+        self.update_derived_stats()
+
+    @property
+    def intel(self):
+        """Returns intelligence"""
+        return self.__intel
+
+    @intel.setter
+    def intel(self, value):
+        self.__intel = value
+        self.update_derived_stats()
+
+    @property
+    def dexterity(self):
+        """Returns dexterity"""
+        return self.__dexterity
+
+    @dexterity.setter
+    def dexterity(self, value):
+        self.__dexterity = value
+        self.update_derived_stats()
+
+    def update_derived_stats(self):
         """
         Generate derived stats based on base stats.
+
         Health int 50-100, mana int 50-100, regen int 1-3, armor int 0-10, damage multiplier float 0-1
         """
         self.health = 50 + int(50 * (self.strength / self.STAT_MAX * self.vitality / self.STAT_MAX))
         self.base_health = self.health
-        self.health_regen = 1 + int(2 * (self.strength / self.STAT_MAX + self.vitality / self.STAT_MAX))
+        self.health_regen = 1 + int(self.strength / self.STAT_MAX + self.vitality / self.STAT_MAX)
         self.mana = 50 + int(50 * (self.intel / self.STAT_MAX * self.vitality / self.STAT_MAX))
         self.base_mana = self.mana
-        self.mana_regen = 1 + int(2 * (self.intel / self.STAT_MAX + self.vitality / self.STAT_MAX))
+        self.mana_regen = 1 + int(self.intel / self.STAT_MAX + self.vitality / self.STAT_MAX)
         self.armor = int(10 * (self.strength / self.STAT_MAX * self.dexterity / self.STAT_MAX))
-        self.damage_multiplier = 1 + self.strength / self.STAT_MAX * self.dexterity / self.STAT_MAX
+        self.base_armor = self.armor
+        self.damage_multiplier = 1 + self.strength / self.STAT_MAX * self.intel / self.STAT_MAX
 
-        self.regen(True)  # Turn on health and mana regeneration by default
-
-    def __str__(self):
-        char_info = f"""
-            strength     {self.strength}
-            vitality     {self.vitality}
-            intelligence {self.intel}
-            dexterity    {self.dexterity}
-            health       {self.health}
-            health regen {self.health_regen}
-            mana         {self.mana}
-            mana regen   {self.mana_regen}
-            armor        {self.armor}
-            damage x     {self.damage_multiplier}
-            items        {self.items}
-        """
-        return char_info
-
-    def _regen_helper(self):
-        """Regenerates health and mana."""
-        self.health = min(self.health + self.health_regen, self.base_health)
-        self.mana = min(self.mana + self.mana_regen, self.base_mana)
-
-    def regen(self, active: bool = True):
-        """Turns regeneration on (True) or off (False)"""
-        if active:
-            self.rt = RepeatedTimer(self.REGEN_INTERVAL, self._regen_helper)
+    def toggle_regen(self, state: bool = None):
+        """Turns regeneration on or off (True/False) or toggles current value with no parameter"""
+        if state is None:
+            self._regen = not self._regen
+        elif state:
+            self._regen = True
         else:
-            self.rt.stop()
+            self._regen = False
 
+    def regen(self):
+        """Update health and mana if regen is active"""
+        if self._regen:
+            self.health = min(self.health + self.health_regen, self.base_health)
+            self.mana = min(self.mana + self.mana_regen, self.base_mana)
 
-class RepeatedTimer(object):
-    """
-    Used to automatically run a function at selected intervals.
-
-    Taken from https://stackoverflow.com/a/13151299/19545063
-    """
-
-    def __init__(self, interval, function, *args, **kwargs):
-        self._timer = None
-        self.interval = interval
-        self.function = function
-        self.args = args
-        self.kwargs = kwargs
-        self.is_running = False
-        self.start()
-
-    def _run(self):
-        self.is_running = False
-        self.start()
-        self.function(*self.args, **self.kwargs)
-
-    def start(self):
-        """Start the timed function"""
-        if not self.is_running:
-            self._timer = Timer(self.interval, self._run)
-            self._timer.start()
-            self.is_running = True
-
-    def stop(self):
-        """Stop the timed function"""
-        self._timer.cancel()
-        self.is_running = False
+    def update(self):
+        """Update character stats and position"""
+        self.regen()
+        self.x += self.dx
+        self.y += self.dy
 
 
 if __name__ == '__main__':
-    from time import sleep
+    player = Character()
+    print('Initial character stats')
+    print(player)
 
-    player1 = Character()
-    print(player1)
+    # Test player property and stat manipulation
+    print('Adding +5 strength and recalculating attributes')
+    player.strength += 5
+    print(player)
 
-    # Test regeneration
-    player1.health = 10
-    player1.mana = 10
-    print('regen is ON by default')
-    for _ in range(10):
-        print(f'health: {player1.health} mana: {player1.mana}')
-        sleep(1.0)
-    player1.regen(False)
-    print('\nregen is now turned OFF')
-    for _ in range(5):
-        print(f'health: {player1.health} mana: {player1.mana}')
-        sleep(1.0)
-    player1.health = player1.base_health - 5
-    player1.mana = player1.base_mana - 5
-    player1.regen(True)
-    print('\nregen ON and stops at full health/mana')
-    for _ in range(7):
-        print(f'health: {player1.health} mana: {player1.mana}')
-        sleep(1.0)
-    print('\nTurn regen OFF when finished to ensure clean exit')
-    player1.regen(False)
+    # Test regen
+    player.health, player.mana = 10, 10
+    print(f'Player health {player.health} mana {player.mana} before update')
+    player.update()
+    print(f'Player health {player.health} mana {player.mana} after update with regen {player._regen}')
+    assert player.health > 10 and player.mana > 10
+    player.toggle_regen()
+    print(f'Player health {player.health} mana {player.mana} after update with regen {player._regen}')
