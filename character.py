4dae2eae812eb06694ce2d950fcf7457d0646aaa
TuplexGaming <99618518+TuplexGaming@users.noreply.github.com>
2022-07-26 20:50:26 -0600

    Add properties, setters for x, y, dx, dy

--- character.py
+++ character.py
@@ -18,23 +18,23 @@ class Character:
         self,
         x: int = 0,
         y: int = 0,
-        dx: int = 0,
-        dy: int = 0,
+        dx: float = 0,
+        dy: float = 0,
         strength: int = None,
         vitality: int = None,
         intel: int = None,
         dexterity: int = None,
         items: list = []
-    ):
+    ) -> None:
         """
         Character initialization.  Generates random base stats if not provided upon creating the class instance.
 
         Base stats should be integers between 3 and 18 (3d6).
         """
-        self.x = x
-        self.y = y
-        self.dx = dx
-        self.dy = dy
+        self.__x = x
+        self.__y = y
+        self.__dx = dx
+        self.__dy = dy
         self.__strength = self.roll_stat(strength)
         self.__vitality = self.roll_stat(vitality)
         self.__intel = self.roll_stat(intel)
@@ -55,100 +55,151 @@ class Character:
         """
         return char_info
 
-    def roll_stat(self, stat):
+    def roll_stat(self, stat: int) -> int:
         """Rolls 3d6 stat value if none is provided"""
         return stat if stat is not None else randint(1, 6) + randint(1, 6) + randint(1, 6)
 
     @property
-    def strength(self):
+    def x(self) -> int:
+        """Returns x coordinate as integer"""
+        return int(self.__x)
+
+    @x.setter
+    def x(self, value: int) -> None:
+        """Set x coordinate"""
+        self.__x = value
+
+    @property
+    def y(self) -> int:
+        """Returns y coordinate as integer"""
+        return int(self.__y)
+
+    @y.setter
+    def y(self, value: int) -> None:
+        """Set y coordinate"""
+        self.__y = value
+
+    @property
+    def dx(self) -> float:
+        """Returns dx"""
+        return self.__dx
+
+    @dx.setter
+    def dx(self, value: float) -> None:
+        """Set dx"""
+        self.__dx = value
+
+    @property
+    def dy(self) -> float:
+        """Returns dy"""
+        return self.__dy
+
+    @dy.setter
+    def dy(self, value: float) -> None:
+        """Set dy"""
+        self.__dy = value
+
+    @property
+    def strength(self) -> int:
         """Returns strength"""
         return self.__strength
 
     @strength.setter
-    def strength(self, value):
+    def strength(self, value: int) -> None:
+        """Sets strength"""
         self.__strength = value
         self.update_derived_stats()
 
     @property
-    def vitality(self):
+    def vitality(self) -> int:
         """Returns vitality"""
         return self.__vitality
 
     @vitality.setter
-    def vitality(self, value):
+    def vitality(self, value: int) -> None:
+        """Sets vitality"""
         self.__vitality = value
         self.update_derived_stats()
 
     @property
-    def intel(self):
+    def intel(self) -> int:
         """Returns intelligence"""
         return self.__intel
 
     @intel.setter
-    def intel(self, value):
+    def intel(self, value: int) -> None:
+        """Sets intelligence"""
         self.__intel = value
         self.update_derived_stats()
 
     @property
-    def dexterity(self):
+    def dexterity(self) -> int:
         """Returns dexterity"""
         return self.__dexterity
 
     @dexterity.setter
-    def dexterity(self, value):
+    def dexterity(self, value: int) -> None:
+        """Sets dexterity"""
         self.__dexterity = value
         self.update_derived_stats()
 
     @property
-    def items(self):
+    def items(self) -> list:
         """Returns list of character's items"""
         return self.__items
 
-    def add_item(self, item):
-        """Adds item to player inventory if not already there (should we allow multiples?)"""
+    def add_item(self, item: object) -> bool:
+        """Adds item to player inventory if not already there, returns True if added, False if already there"""
         if item not in self.items:
             self.items.append(item)
+            return True
+        return False
 
-    def remove_item(self, item):
-        """Removes item from player inventory"""
+    def remove_item(self, item: object) -> bool:
+        """Removes item from player inventory, returns True if successful, False if item not in inventory"""
         if item in self.items:
             self.items.remove(item)
+            return True
+        return False
 
-    def update_derived_stats(self):
+    def update_derived_stats(self) -> None:
         """
         Generate derived stats based on base stats.
 
-        Health int 50-100, mana int 50-100, regen int 1-3, armor int 0-10, damage multiplier float 0-1
+        Health float 50-100, mana float 50-100, regen float 1-3, armor float 0-10, damage multiplier float 0-1
         """
-        self.health = 50 + int(50 * (self.strength / self.STAT_MAX * self.vitality / self.STAT_MAX))
+        self.health = 50 + 50 * (self.strength / self.STAT_MAX * self.vitality / self.STAT_MAX)
         self.base_health = self.health
-        self.health_regen = 1 + int(self.strength / self.STAT_MAX + self.vitality / self.STAT_MAX)
-        self.mana = 50 + int(50 * (self.intel / self.STAT_MAX * self.vitality / self.STAT_MAX))
+        self.health_regen = 1 + self.strength / self.STAT_MAX + self.vitality / self.STAT_MAX
+        self.mana = 50 + 50 * (self.intel / self.STAT_MAX * self.vitality / self.STAT_MAX)
         self.base_mana = self.mana
-        self.mana_regen = 1 + int(self.intel / self.STAT_MAX + self.vitality / self.STAT_MAX)
-        self.armor = int(10 * (self.strength / self.STAT_MAX * self.dexterity / self.STAT_MAX))
+        self.mana_regen = 1 + self.intel / self.STAT_MAX + self.vitality / self.STAT_MAX
+        self.armor = 10 * (self.strength / self.STAT_MAX * self.dexterity / self.STAT_MAX)
         self.base_armor = self.armor
         self.damage_multiplier = 1 + self.strength / self.STAT_MAX * self.intel / self.STAT_MAX
 
-    def toggle_regen(self, state: bool = None):
-        """Turns regeneration on or off (True/False) or toggles current value with no parameter"""
+    def toggle_regen(self, state: bool = None) -> None:
+        """Turns regeneration on or off (True/False), or toggles current value with no parameter"""
         self._regen = not self._regen if state is None else state
 
-    def regen(self, dt):
+    def regen(self, dt: float) -> None:
         """Update health and mana if regen is active"""
         if self._regen:
-            self.health = min(self.health + int(dt * self.health_regen), self.base_health)
-            self.mana = min(self.mana + int(dt * self.mana_regen), self.base_mana)
+            self.health = min(self.health + dt * self.health_regen, self.base_health)
+            self.mana = min(self.mana + dt * self.mana_regen, self.base_mana)
 
-    def update(self, dt: float):
+    def update(self, dt: float) -> None:
         """
         Update character stats and position.
 
         Parameter dt is elapsed time since last update, in seconds.
+        Returns True if health <= 0
         """
         self.regen(dt)
-        self.x += int(dt * self.dx)
-        self.y += int(dt * self.dy)
+        self.__x += dt * self.__dx
+        self.__y += dt * self.__dy
+
+        return self.health <= 0
 
 
 if __name__ == '__main__':
